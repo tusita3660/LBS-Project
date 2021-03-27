93de2ccb1626f5ab7db9e2668c2321ad53fc8d73
tusita3660 <42643065+tusita3660@users.noreply.github.com>
2021-03-27 16:42:14 +0530

    Add files via upload

---
+++ basic_imports.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import networkx as nx
+import numpy as np
+import random as r
+import osmnx as ox
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+
+
